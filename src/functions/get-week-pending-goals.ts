3c00e058889a00e942d160ca34364e1b8998238e
Anderson Santana <andersonsantana.developer@gmail.com>
2024-09-14 18:55:50 -0300

    AULA01 - Completa

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -1,15 +1,16 @@
 import dayjs from 'dayjs'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 import { db } from '../db'
-import { and, lte } from 'drizzle-orm'
-import { goals } from '../db/schema'
+import { and, lte, count, sql, gte, eq } from 'drizzle-orm'
+import { goals, goalCompletions } from '../db/schema'
 
 dayjs.extend(weekOfYear)
 
 export function getWeekPendingGoals() {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
   const lastDayOfWeek = dayjs().endOf('week').toDate()
 
-  const goalsCreatedUpToWeek = db.$with('goalsCreatedUpToWeek').as(
+  const goalsCreatedUpToWeek = db.$with('goal_created_up_to_week').as(
     db
       .select({
         id: goals.id,
@@ -20,4 +21,38 @@ export function getWeekPendingGoals() {
       .from(goals)
       .where(lte(goals.createdAt, lastDayOfWeek))
   )
+
+  const goalCompletionsCounts = db.$with('goal_completions_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWeek)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const pendinGoals = db
+    .with(goalsCreatedUpToWeek, goalCompletionsCounts)
+    .select({
+      id: goalsCreatedUpToWeek.id,
+      title: goalsCreatedUpToWeek.title,
+      desiredWeeklyFrequency: goalsCreatedUpToWeek.desiredWeeklyFrequency,
+      completionCount: sql`
+    COALESCE(${goalCompletionsCounts.completionCount}, 0)
+    `.as('completionCount'),
+    })
+    .from(goalsCreatedUpToWeek)
+    .leftJoin(
+      goalCompletionsCounts,
+      eq(goalCompletionsCounts.goalId, goalsCreatedUpToWeek.id)
+    )
+
+  return pendinGoals
 }
